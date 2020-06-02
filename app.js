88405ac272abd41258518c845fefd48aa50f2bde
hunghly <hunghly@gmail.com>
2020-06-01 19:23:00 -0500

    worked on login feature, added middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path'); // tool used to set file path
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const Moment = require('moment');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,6 +17,19 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'))); // uses the public directory '
 app.use(bodyParser.urlencoded());
 
+// added middleware aka like an extra servlet that handles responses before passing it on to the next.
+app.use((req, res, next)=>{
+    console.log('FIRST MIDDLEWARE!');
+    next();
+})
+app.use((req, res, next)=> {
+    console.log("SECOND MIDDLEWARE")
+    req.timestamp = Moment().format();
+    // res.json({
+    //     data: "stuff"
+    // });
+    next();
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -3,78 +3,102 @@ const router = express.Router();
 const Moment = require('moment');
 
 const profiles = [
-  {name: 'Mike', city: 'San Antonio'},
-  {name: 'Hung', city: 'Dallas', profession: 'coder'},
-  {name: 'Tatiana', city: 'Carolina', profession: 'chemist'}
+    {name: 'Mike', city: 'San Antonio'},
+    {name: 'Hung', city: 'Dallas', profession: 'coder'},
+    {name: 'Tatiana', city: 'Carolina', profession: 'chemist'}
 ];
+
+let user = null;
 
 /* GET home page. */
 // returns the index html
-router.get('/', function(req, res, next) {
-  // res.render('index.hjs', { title: 'Express' });
-  const data = {
-    name: 'Hung',
-    time: Moment().format(),
-    title: 'My Application',
-    profiles: profiles
-  }
-  res.render('index', data);
+router.get('/', function (req, res, next) {
+    // res.render('index.hjs', { title: 'Express' });
+    if (!user) {
+        res.redirect('/login');
+    }
+    const data = {
+        name: 'Hung',
+        time: req.timestamp,
+        title: 'My Application',
+        profiles: profiles,
+        user: user
+    }
+    res.something = 'hello there';
+    res.render('index', data);
 });
 
 // Create a post handler for our form
-router.post("/join", function(req, res, next) {
-  const body = req.body;
-  const params = req.param();
-  profiles.push(body);
-  res.redirect('/');
-  // console.log(body);
-  // console.log(params);
-  // res.json({
-  //     data: "This is the Post Request Handler",
-  //   body: body
-  // });
+router.post("/join", function (req, res, next) {
+    const body = req.body;
+    const params = req.param();
+    profiles.push(body);
+    res.redirect('/');
+    // console.log(body);
+    // console.log(params);
+    // res.json({
+    //     data: "This is the Post Request Handler",
+    //   body: body
+    // });
 });
 
+router.get('/login', function(req, res, next) {
+    res.render('login', null);
+})
+
+router.post('/login', (req, res, next)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    if (password === '123') {
+        user = {username, password};
+        res.redirect('/');
+        return;
+    }
+    res.json({
+        data: 'failed login'
+    });
+})
+
 router.get('/json', function (req, res, next) {
-  const data = {
-    'text': 'hello there!',
-    'name': 'hung',
-    'location': 'san antonio'
-  }
-  res.json(data);
+    const data = {
+        'text': 'hello there!',
+        'name': 'hung',
+        'location': 'san antonio',
+        time: req.timestamp
+    }
+    res.json(data);
 })
 router.get('/html', function (req, res, next) {
-  const html =
-      `<html>
+    const html =
+        `<html>
         <h1 style="color: red">This is an HTML response</h1>
         </html>`;
-  res.send(html);
+    res.send(html);
 })
 // get query
 router.get('/query', function (req, res, next) {
-  const query = req.query;
-  console.log(query);
-  res.json(query);
+    const query = req.query;
+    console.log(query);
+    res.json(query);
 })
 // get params passed in
 router.get('/params/:name/:location/:occupation', function (req, res, next) {
-  const params = req.params;
-  res.json({
-    'params': params
-  });
+    const params = req.params;
+    res.json({
+        'params': params
+    });
 })
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  const data = {
-    name: 'Home',
-    date: 'May 12, 2020'
-  }
-  res.render('index.hjs', data);
+    const data = {
+        name: 'Home',
+        date: 'May 12, 2020'
+    }
+    res.render('index.hjs', data);
 
-  // res.render('test', null)
+    // res.render('test', null)
 });
-
 
 
 module.exports = router;
